006fd6571fc3084f575fb3359e4c647150abc74f
sumitkumar9263 <164641331+sumitkumar9263@users.noreply.github.com>
2024-06-27 02:30:42 +0000

    commit

---
+++ .devcontainer/Basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+
+//allert( 3 + 3) //we are using node js not browser
+
+
+console.log(3 
+    
+    + 3) // code readability should be high
+
+ console.log("sumit")
+
+ let name = "sumit"
+ let age = 18
+ let isLogedIn = false
+
+ //number => 2 to power 53
+ //bigint 
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined => 
+ //symbol => unique
+
+
+  //object
+
+  console.log(typeof age)  //prints number
+  console.log(typeof null) //prints object
+  console.log(typeof undefined) //prints undefined
+
+
